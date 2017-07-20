176839b4c17d8d44f192f97805f1164a5d7c5b6e
Mechanicalbird <mechanicalbird_just@yahoo.com>
2017-07-21 00:28:08 +0900

    Create python.py

---
+++ python.py
@@ -0,0 +1,23 @@
+x = 5**3.0
+y = x+5.0
+print x , y
+
+print "are you stupid+*}{`?_>+**[@@[]]]]]]:;./\!"
+
+a = 'ahmad'
+b = 'mohammad'
+c = a+"  " + b
+
+print a,"  ",b, " ", c
+print ('\n')
+print (r'\n')
+print a*5
+print a[:]
+print len(b)
+
+pl = [164,210,35,15]
+pl[1] = 10
+pl2 = [164,210,35,15]
+pl.append(120)
+pl[:]=[]
+print pl
