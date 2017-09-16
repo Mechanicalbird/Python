867a271d17163bac03ceaef12d0f824cf2c04bbe
Mechanicalbird <mechanicalbird_just@yahoo.com>
2017-09-17 07:00:27 +0900

    Add files via upload

---
+++ stu-jp7.py
@@ -0,0 +1,9 @@
+def add_number(*args):
+    total = 0 
+    for a in args:
+     total += a
+    print(total)
+
+
+add_number(9)
+add_number(3,3,3,3,4,3,3,4,40000)
---
+++ stu-jp8.py
@@ -0,0 +1,6 @@
+classmates = {"tony": ' man1111',"watwat1": '  man', "ashish": 'my man'}
+
+
+
+for k, v in classmates.items():
+     print (k + v)
